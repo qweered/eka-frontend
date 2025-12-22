3d01f7db3649d996b903f920ff332b9bbd904385
Aliaksandr <grubian2@gmail.com>
2025-12-22 16:47:24 +0200

    prevent flash after font loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import tailwindcss from '@tailwindcss/vite'
-// oxlint-disable no-default-export
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
+// oxlint-disable-next-line no-default-export
 export default defineConfig({
   base: '/eka-frontend',
   plugins: [react(), tailwindcss()],
